6d55412b42fd4ca9e010b549b1ef21bc8784f284
Alena Timashova <alyona8808@mail.ru>
2024-01-29 00:57:44 +0300

    seminar 4 js hw  added

---
+++ GB_seminars/seminar4_hw/script.js
@@ -0,0 +1,45 @@
+//Необходимо получить список всех пользователей с помощью бесплатного API (https://jsonplaceholder.typicode.com/users)
+// и отобразить их на странице.
+
+const URL = "https://jsonplaceholder.typicode.com/users";
+const userList = document.querySelector(".myList");
+
+async function myAsync(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+try {
+  const myData = await myAsync(URL);
+  myData.forEach((el) => {
+    userList.insertAdjacentHTML(
+      "beforeend",
+      `
+                    <li>${el.name}</li>
+         `
+    );
+  });
+} catch (error) {
+  console.log(error.message);
+}
+
+//Необходимо реализовать отрисовку 10 картинок собак из API https://dog.ceo/dog-api/ с интервалом в 3 секунды.
+
+const dogAPI = "https://dog.ceo/api/breeds/image/random";
+const imgContainer = document.querySelector(".img-container");
+
+async function showImg() {
+  let myData1 = await myAsync(dogAPI);
+  imgContainer.insertAdjacentHTML(
+    "beforeend",
+    `
+    <img class="dogImg" src="${myData1.message}">
+           `
+  );
+}
+
+let interval = async () => {
+  setInterval(showImg, 3000);
+};
+interval();
