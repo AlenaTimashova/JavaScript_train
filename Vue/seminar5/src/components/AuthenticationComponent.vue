16e4fe2a633c6506c6d3cdf197bf0478f588b98f
Alena Timashova <alyona8808@mail.ru>
2024-03-06 19:15:29 +0300

    seminar 5 added

---
+++ Vue/seminar5/src/components/AuthenticationComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <form v-if="isAuth">
+    <input type="text" placeholder="login" />
+    <input type="password" placeholder="password" />
+    <button>Submit</button>
+  </form>
+  <div v-else>
+    <h1>Вы зарегистрированы успешно</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isAuth: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
