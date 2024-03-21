6107ed456217fae2ce34cbabd1637c82289e2eda
Alena Timashova <alyona8808@mail.ru>
2024-03-21 19:28:02 +0300

    seminars tasks added

--- Vue/seminar5/src/App.vue
+++ Vue/seminar5/src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <BookListComponent />
-  <MovieListComponent />
+  <!-- <BookListComponent />
+  <MovieListComponent /> -->
+  <!-- <CounterComponent /> -->
+  <NavComponent />
+  <RouterView />
 </template>
 
 <script>
-import BookListComponent from "./components/BookListComponent.vue";
-import MovieListComponent from "./components/MovieListComponent.vue";
+// import BookListComponent from "./components/BookListComponent.vue";
+// import MovieListComponent from "./components/MovieListComponent.vue";
+// import CounterComponent from "./components/CounterComponent.vue";
+import NavComponent from "./components/NavComponent.vue";
 
 export default {
   name: "App",
   components: {
-    BookListComponent,
-    MovieListComponent,
+    // BookListComponent,
+    // MovieListComponent,
+    // CounterComponent,
+    NavComponent,
   },
 };
 </script>
